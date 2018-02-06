871d013e80931b7c0a85ac2361c788a53c6fff54
NetworkingJoe <34258193+NetworkingJoe@users.noreply.github.com>
2018-02-05 20:19:17 -0800

    Update apacheupdate2.sh
    Added a Colon

--- apacheupdate2.sh
+++ apacheupdate2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-sudo echo -e "<html> \n<h1> Welcome, NTI-300 </h1>  \n<h2> Hello, my name is Ignego Montoya, you killed my father, prepare to die! </h2> \n <p> The current date is $(date +%c)</p> \n</html>" > /var/www/html/index.html
+sudo echo -e "<html> \n<h1> Welcome, NTI-300 </h1>  \n<h2> Hello, my name is Ignego Montoya, you killed my father, prepare to die! </h2> \n <p> The current date is: $(date +%c)</p> \n</html>" > /var/www/html/index.html
 
