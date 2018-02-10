371c7828886c112c5bcbe79b8fdafd3fb2ec5223
NetworkingJoe <paulierev1775@gmail.com>
2018-02-10 16:38:12 +0000

    "I am modifying my catstatement to add in my own words"

--- catstatement.sh
+++ catstatement.sh
@@ -2,6 +2,6 @@ cat <<EOF> test.sh
 
 #!/bin/bash
 
-echo "This is the best way to echo in a config file because WYSIWYG"
+echo "This is the best way to echo in a config file because WYSIWYG. Oh and here is my own comments for this file, because, as the lego movie puts it...Everything Is Awesome"
 
 EOF
