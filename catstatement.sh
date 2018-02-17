d428b87b819e48f3d66d219e8ff7219a0e670a72
NetworkingJoe <paulierev1775@gmail.com>
2018-02-17 03:35:04 +0000

    changed and fixed #!/bin/bash line in cat statement.

--- catstatement.sh
+++ catstatement.sh
@@ -1,6 +1,6 @@
-cat <<EOF> test.sh
-
 #!/bin/bash
+
+cat <<EOF> test.sh
 
 echo "This is the best way to echo in a config file because WYSIWYG. Oh and here is my own comments for this file, because, as the lego movie puts it...Everything Is Awesome"
 
