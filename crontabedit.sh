eb62439df9ef3aed63fab45dc799361edb045dd0
NetworkingJoe <paulierev1775@gmail.com>
2018-02-17 03:16:40 +0000

    I added a code to my crontab file

--- crontabedit.sh
+++ crontabedit.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+sudo echo -e "<html> \n<h1> Welcome, NTI-300 </h1>  \n<h2> Hello, my name is Ignego Montoya, you killed my father, prepare to die!</h2> <p> The current date and time is: $(date +%c)</p> \n</html>" > /var/www/html/index.html
+
 # ┌───────────── minute (0 - 59)
 # │ ┌───────────── hour (0 - 23)
 # │ │ ┌───────────── day of month (1 - 31)
